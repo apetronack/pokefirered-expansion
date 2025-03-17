6c4a8dfbcd894641cfe7d8e197b4c546e4206a54
Austin Petronack <austin.petronack@nextern.com>
2025-03-17 15:42:26 -0500

    Updating trainer typos, tweaking early encounter levels, and cutting experience in half for all level up types

--- src/data/pokemon/experience_tables.h
+++ src/data/pokemon/experience_tables.h
@@ -1,19 +1,19 @@
 #define SQUARE(n)((n) * (n))
 #define CUBE(n)((n) * (n) * (n))
 
-#define EXP_SLOW(n)((5 * CUBE(n)) / 4) // (5 * (n)^3) / 4
-#define EXP_FAST(n)((4 * CUBE(n)) / 5) // (4 * (n)^3) / 5
-#define EXP_MEDIUM_FAST(n)(CUBE(n)) // (n)^3
-#define EXP_MEDIUM_SLOW(n)((6 * CUBE(n)) / 5 - (15 * SQUARE(n)) + (100 * n) - 140)    // (6 * (n)^3) / 5 - (15 * (n)^2) + (100 * n) - 140
+#define EXP_SLOW(n)((5 * CUBE(n)) / 8) // (5 * (n)^3) / 8
+#define EXP_FAST(n)((4 * CUBE(n)) / 10) // (4 * (n)^3) / 10
+#define EXP_MEDIUM_FAST(n)(CUBE(n) / 2) // (n)^3 / 2
+#define EXP_MEDIUM_SLOW(n)((6 * CUBE(n)) / 10 - (7 * SQUARE(n)) + (50 * n) - 70)    // (6 * (n)^3) / 10 - (7 * (n)^2) + (50 * n) - 70
 #define EXP_ERRATIC(n)                                      \
-     (n <= 50) ? ((100 - n) * CUBE(n) /  50)                \
-    :(n <= 68) ? ((150 - n) * CUBE(n) / 100)                \
-    :(n <= 98) ? (((1911 - 10 * n) / 3) * CUBE(n) / 500)    \
+     (n <= 50) ? ((100 - n) * CUBE(n) /  100)                \
+    :(n <= 68) ? ((150 - n) * CUBE(n) / 200)                \
+    :(n <= 98) ? (((1911 - 10 * n) / 3) * CUBE(n) / 1000)    \
     :            ((160 - n) * CUBE(n) / 100)
 #define EXP_FLUCTUATING(n)                                  \
-     (n <= 15) ? (((n + 1) / 3 + 24) * CUBE(n) / 50)        \
-    :(n <= 36) ? ((n + 14)           * CUBE(n) / 50)        \
-    :            (((n / 2) + 32)     * CUBE(n) / 50)
+     (n <= 15) ? (((n + 1) / 3 + 24) * CUBE(n) / 100)        \
+    :(n <= 36) ? ((n + 14)           * CUBE(n) / 100)        \
+    :            (((n / 2) + 32)     * CUBE(n) / 100)
 
 const u32 gExperienceTables[][MAX_LEVEL + 1] =
 {
