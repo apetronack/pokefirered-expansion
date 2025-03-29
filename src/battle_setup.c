9bdacf3930524e3c3baea967b96f0ab2cd586c79
apetronack <apetronack@gmail.com>
2025-03-29 08:57:32 -0700

    Removing subset of pokemon from game. Need to do more to test make and edit trainers, encounters

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -245,7 +245,7 @@ static void DoTrainerBattle(void)
 
 void StartOldManTutorialBattle(void)
 {
-    CreateMaleMon(&gEnemyParty[0], SPECIES_WEEDLE, 5);
+    CreateMaleMon(&gEnemyParty[0], SPECIES_BELDUM, 5);
     LockPlayerFieldControls();
     gMain.savedCallback = CB2_ReturnToFieldContinueScriptPlayMapMusic;
     gBattleTypeFlags = BATTLE_TYPE_OLD_MAN_TUTORIAL;
