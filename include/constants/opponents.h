8abd818b7acd80380511b59ee63943bd74a2e366
apetronack <apetronack@gmail.com>
2025-04-22 12:56:28 -0700

    Update to block gyms to enforce game progression order Converting to poryscript throughout

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -8,11 +8,11 @@
 #define TRAINER_AQUA_LEADER                        1
 #define TRAINER_AQUA_GRUNT_M                       2
 #define TRAINER_AQUA_GRUNT_F                       3
-#define TRAINER_RS_AROMA_LADY                      4
-#define TRAINER_RS_RUIN_MANIAC                     5
-#define TRAINER_INTERVIEWER                        6
-#define TRAINER_RS_TUBER_F                         7
-#define TRAINER_RS_TUBER_M                         8
+#define TRAINER_CERULEAN_GYM_GUARDIAN              4
+#define TRAINER_POKEMON_TOWER_GUARDIAN             5
+#define TRAINER_CELADON_GYM_GUARDIAN               6
+#define TRAINER_FUSCHIA_GYM_GUARDIAN               7
+#define TRAINER_CINNABAR_MANSION_GUARDIAN          8
 #define TRAINER_RS_COOLTRAINER_M                   9
 #define TRAINER_RS_COOLTRAINER_F                  10
 #define TRAINER_HEX_MANIAC                        11
