c78c99899f5a3ee54afa44bc6e8062702ba27161
Austin Petronack <apetronack@gmail.com>
2025-07-22 15:31:19 -0500

    Having Heidi give super potions before rock tunnel

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -809,7 +809,7 @@
 #define FLAG_BEAT_IRIS           0x30B // Beat Iris and received potions in Mt Moon
 #define FLAG_BEAT_RICK           0x30C
 #define FLAG_BEAT_THOMAS         0x30D
-#define FLAG_0x30E               0x30E
+#define FLAG_BEAT_HEIDI          0x30E
 #define FLAG_0x30F               0x30F
 #define FLAG_0x310               0x310
 #define FLAG_0x311               0x311
