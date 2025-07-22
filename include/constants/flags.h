c8ba6e93d031eac18446b7589c6f3f4decad78d5
Austin Petronack <apetronack@gmail.com>
2025-07-22 14:33:34 -0500

    Attempting to add potion gift to first trainer in Mt Moon (Iris)

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -806,7 +806,7 @@
 #define FLAG_HIDE_CINNABAR_MANSION_GUARDIAN          0x308
 #define FLAG_GOT_TM52_FROM_KOICHI                    0x309
 #define FLAG_GOT_TM65            0x30A
-#define FLAG_0x30B               0x30B
+#define FLAG_BEAT_IRIS           0x30B // Beat Iris and received potions in Mt Moon
 #define FLAG_0x30C               0x30C
 #define FLAG_0x30D               0x30D
 #define FLAG_0x30E               0x30E
