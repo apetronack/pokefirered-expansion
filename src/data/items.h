2d130b1bc24371edf91621b197d736133cfdfc60
Austin Petronack <apetronack@gmail.com>
2025-10-30 06:29:46 -0500

    Changing Dowsing Machine in-game name to itemfinder

--- src/data/items.h
+++ src/data/items.h
@@ -12424,7 +12424,7 @@ const struct Item gItemsInfo[] =
 
     [ITEM_DOWSING_MACHINE] =
     {
-        .name = _("Dowsing MCHN"),
+        .name = _("Itemfinder"),
         .price = 0,
         .description = COMPOUND_STRING(
             "A device that "
